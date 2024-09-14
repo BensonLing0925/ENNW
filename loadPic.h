564a955af60ac3b8b5d8b25eaf5cbc6297efa2aa
BensonLing0925 <117259236+BensonLing0925@users.noreply.github.com>
2024-09-14 14:43:20 +0800

    Add files via upload

--- loadPic.h
+++ loadPic.h
@@ -3,13 +3,272 @@
 #include <stdlib.h>
 #include "structDef.h"
 
-uint32_t littleToBigEndian(uint32_t little) {
+#define BUFSIZE 2048
+#define FILELEN 256
+#define H_OFFSET 5
+#define W_OFFSET 7
+
+#define SOS 0xDA
+#define SOF0 0xC0
+#define DQT 0xDB
+#define DHT 0xC4
+#define SOI 0xD8
+#define EOI 0xD9
+
+const uint8_t SOS_MARKER[2] = { 0xFF, 0xDA };
+const uint8_t SOF0_MARKER[2] = { 0xFF, 0xC0 };
+const uint8_t DQT_MARKER[2] = { 0xFF, 0xDB };
+const uint8_t DHT_MARKER[2] = { 0xFF, 0xC4 };
+const uint8_t SOI_MARKER[2] = { 0xFF, 0xD8 };
+const uint8_t EOI_MARKER[2] = { 0xFF, 0xD9 };
+
+typedef enum {
+	LUMINANCE = 0,	
+	CHROMINANCE = 1
+} Component;
+
+typedef enum {
+	DC = 0,	
+	AC = 1
+} TableClass;
+
+typedef struct {
+	Component component;	
+	uint8_t precision;
+	int dqt_len;
+	union {
+		uint8_t* table8;
+		uint16_t* table16;
+	};
+} DQT_struct;
+
+typedef struct {
+	TableClass tClass;
+	uint8_t precision;
+	int dht_len;
+	union {
+		uint8_t* table8;
+		uint16_t* table16;
+	};
+} DHT_struct;	
+
+typedef struct {
+	int pic_width;
+	int pic_height;
+	uint8_t*** picture;
+	int num_DQT;
+	int num_DHT;
+	DQT_struct* DQTs;
+	DHT_struct* DHTs;
+} Img;	
+
+uint32_t littleToBigEndian32(uint32_t little) {
 	return ((little >> 24) & 0x000000FF) |
 		   ((little >> 8)  & 0x0000FF00) |
 		   ((little << 8)  & 0x00FF0000) |
 		   ((little << 24) & 0xFF000000);
 }
-		   
+
+uint16_t littleToBigEndian16(uint16_t little) {
+    return ((little >> 8) & 0x00FF) |
+           ((little << 8) & 0xFF00);
+}
+
+uint16_t byteConcat(uint8_t byte1, uint8_t byte2) {
+	return (byte1 << 8 | byte2);
+}		
+
+void readTableInfo(FILE* fptr, Img* image) {
+	size_t size_read;
+	uint8_t marker;
+	uint8_t read_byte;
+	uint16_t read_2bytes;
+	int i_dqt = 0;
+	int i_dht = 0;
+	while ( (size_read = fread(&read_byte, 1, 1, fptr)) > 0 ) {
+		if ( read_byte == 0xFF ) {
+			fread(&marker, 1, 1, fptr);
+			fread(&read_2bytes, 1, 2, fptr);
+			read_2bytes = (read_2bytes << 8) | (read_2bytes >> 8);
+			fread(&read_byte, 1, 1, fptr);
+			switch (marker) {
+				case DQT:
+					image->DQTs[i_dqt].dqt_len = read_2bytes;
+					image->DQTs[i_dqt].component = (Component)((read_byte << 4) & 0xF0);
+					image->DQTs[i_dqt].precision = ((read_byte >> 4) & 0x01);
+					i_dqt++;
+					break;
+				case DHT:		
+					image->DHTs[i_dht].dht_len = read_2bytes;
+					image->DHTs[i_dht].tClass = (TableClass)((read_byte << 4) & 0xF0);
+					image->DHTs[i_dht].precision = ((read_byte >> 4) & 0x0F);
+					i_dht++;
+					break;
+				}
+		}	
+	}
+	rewind(fptr);
+}	
+
+void readMarkers(FILE* fptr, Img* image) {
+	size_t size_read;
+	uint8_t marker = 0;
+	uint8_t read_byte;
+	while ( (size_read = fread(&read_byte, 1, 1, fptr)) > 0 ) {
+		if ( read_byte == 0xFF ) {
+			fread(&marker, 1, 1, fptr);
+			switch (marker) {
+				case DQT:
+					image->num_DQT++;
+					break;
+				case DHT:
+					image->num_DHT++;
+					break;
+			}		
+		}			
+	}
+	rewind(fptr);
+}	
+
+void printDebug(Img* image) {
+	printf("num_DQT: %d\n", image->num_DQT);
+	printf("num_DHT: %d\n", image->num_DHT);
+
+	/*
+	for ( int i = 0 ; i < image->num_DQT ; ++i ) {
+		printf("DQT LEN: %d\n", image->DQTs[i].dqt_len);
+	}
+	for ( int i = 0 ; i < image->num_DHT ; ++i ) {
+		printf("DQT LEN: %d\n", image->DHTs[i].dht_len);
+	}
+	*/
+
+	printf("===========================\n");
+}	
+
+void initImg(Img* image) {
+	image->num_DQT = 0;
+	image->num_DHT = 0;
+}	
+
+void imageProcess(const char filePath[]) {
+	FILE* fptr = fopen(filePath, "rb");
+	Img image;
+	initImg(&image);
+	readMarkers(fptr, &image);
+	image.DQTs = (DQT_struct*)malloc(image.num_DQT * sizeof(DQT_struct));
+	image.DHTs = (DHT_struct*)malloc(image.num_DHT * sizeof(DHT_struct));
+	readTableInfo(fptr, &image);
+
+	printDebug(&image);
+
+	free(image.DQTs);
+	free(image.DHTs);
+	fclose(fptr);
+}	
+
+/*
+void findTableLen(FILE* fptr, Img* image) {
+	int i_dqt = 0;
+	int i_dht = 0;
+	size_t size_read;
+	uint8_t marker = 0;
+	uint16_t temp = 0;
+	while ((size_read = fread(&marker, 1, 1, fptr)) > 0) {
+		if ( marker == 0xFF ) {
+			fread(&marker, 1, 1, fptr);
+			if (marker == DQT) {
+				fread(&temp, 1, 2, fptr);		
+				image->imgHeader.dqt_len[i_dqt++] = littleToBigEndian16(temp);
+			}		
+			if (marker == DHT) {
+				fread(&temp, 1, 2, fptr);		
+				image->imgHeader.dht_len[i_dht++] = littleToBigEndian16(temp);
+			}		
+		}		
+	}		
+	rewind(fptr);
+}
+void freeTable(Img* image) {
+	for ( int i = 0 ; i < image->imgHeader.num_dqt ; ++i )
+		free(image->imgData.dqt_tables[i]);
+	for ( int i = 0 ; i < image->imgHeader.num_dht ; ++i )
+		free(image->imgData.dht_tables[i]);
+	free(image->imgData.dqt_tables);	
+	free(image->imgData.dht_tables);	
+	free(image->imgHeader.dqt_len);
+	free(image->imgHeader.dht_len);
+}		
+
+void allocTable(Img* image) {
+	image->imgData.dqt_tables = (uint8_t**)malloc(image->imgHeader.num_dqt * sizeof(uint8_t*));	
+	image->imgData.dht_tables = (uint8_t**)malloc(image->imgHeader.num_dht * sizeof(uint8_t*));	
+	for ( int i = 0 ; i < image->imgHeader.num_dqt ; ++i ) {
+		image->imgData.dqt_tables[i] = (uint8_t*)malloc(image->imgHeader.dqt_len[i] * sizeof(uint8_t));
+	}		
+	for ( int i = 0 ; i < image->imgHeader.num_dht ; ++i ) {
+		image->imgData.dht_tables[i] = (uint8_t*)malloc(image->imgHeader.dht_len[i] * sizeof(uint8_t));
+	}		
+}		
+
+void readTable(FILE* fptr, Img* image) {
+	int i_dqt = 0;
+	int i_dht = 0;
+	size_t size_read;
+	uint8_t marker = 0;
+	uint8_t precision, a_byte;
+	uint16_t temp = 0;
+	while ((size_read = fread(&marker, 1, 1, fptr)) > 0) {
+		if ( marker == 0xFF ) {
+			fread(&marker, 1, 1, fptr);
+			fread(&temp, 1, 2, fptr);		// read table length
+			for ( int i = 0 ; i < temp ; ++i ) {
+				fread(&a_byte, 1, 1, fptr);
+				if ( marker == DQT ) 
+					image->imgData.dqt_tables[i_dqt][i] = a_byte;
+				if ( marker == DHT )
+					image->imgData.dht_tables[i_dht][i] = a_byte;
+			}	
+			printf("\n");
+			if ( marker == DQT ) i_dqt++;
+			else i_dht++;
+		}		
+	}		
+}		
+void getJPEGMarker(const char filePath[]) {
+	FILE* fptr = fopen(filePath, "rb");
+	if (!fptr) {
+		printf("File does not exist\n");
+		return;
+	}		
+
+	Img image;
+
+	int iter = 0;
+	uint8_t buffer[BUFSIZE];
+	uint8_t last_byte = 0;
+	size_t size_read;
+
+	rewind(fptr);
+
+	image.imgHeader.dqt_len = (int*)malloc(image.imgHeader.num_dqt * sizeof(int));
+	image.imgHeader.dht_len = (int*)malloc(image.imgHeader.num_dht * sizeof(int));
+	image.imgData.tableType = (int*)malloc(image.imgHeader.num_dqt * sizeof(int));
+
+	findTableLen(fptr, &image);
+
+	for ( int i = 0 ; i < image.imgHeader.num_dqt ; ++i )
+		printf("dqt_len: %d\n", image.imgHeader.dqt_len[i]);
+	for ( int i = 0 ; i < image.imgHeader.num_dht ; ++i )
+		printf("dht_len: %d\n", image.imgHeader.dht_len[i]);
+
+	allocTable(&image);
+	readTable(fptr, &image);
+	freeTable(&image);
+
+	printf("=====================================================\n");
+}		
+	*/	   
 void loadImgLabel(const char filePath[], DataSet* dataset) {
 	FILE* fptr = fopen(filePath, "rb");
 	if (!fptr) {
@@ -22,8 +281,8 @@ void loadImgLabel(const char filePath[], DataSet* dataset) {
 	fread(&magic, 4, 1, fptr);
 	fread(&num_sample, 4, 1, fptr);
 
-	magic = littleToBigEndian(magic);
-	num_sample = littleToBigEndian(num_sample);
+	magic = littleToBigEndian32(magic);
+	num_sample = littleToBigEndian32(num_sample);
 
 	unsigned char temp;
 	for ( int i = 0 ; i < dataset->num_sample ; ++i ) {
@@ -49,10 +308,10 @@ void loadImgFile(const char filePath[], DataSet* dataset, int sampleCnt){
 	fread(&rows, 4, 1, fptr);
 	fread(&cols, 4, 1, fptr);
 
-	magic = littleToBigEndian(magic);
-	num_sample = littleToBigEndian(num_sample);
-	rows = littleToBigEndian(rows);
-	cols = littleToBigEndian(cols);
+	magic = littleToBigEndian32(magic);
+	num_sample = littleToBigEndian32(num_sample);
+	rows = littleToBigEndian32(rows);
+	cols = littleToBigEndian32(cols);
 
 	if ( sampleCnt == -1 ) {
 		dataset->num_sample = num_sample;
@@ -82,4 +341,9 @@ void loadImgFile(const char filePath[], DataSet* dataset, int sampleCnt){
 
 	free(buffer);
 	fclose(fptr);
+}		
+
+void loadKernal(const char filePath[], Conv2D* conv) {
+	
+
 }		
